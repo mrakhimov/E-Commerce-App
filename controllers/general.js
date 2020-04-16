2bfc06277fc9af37530f3fc876066d0b020fc9ce
mrakhimov <mokhinur.rakhimov@gmail.com>
2020-04-16 18:04:47 -0400

    Search functionality implemented

--- controllers/general.js
+++ controllers/general.js
@@ -42,7 +42,8 @@ router.get("/dashboard",isAuthenticated,dashBoardLoader);
 
 // Filter results by category
 router.post("/dashboard", isAuthenticated, (req,res) => {
-    productsModel.find({category: req.body.category})
+    if (req.body.category == "all") {
+        productsModel.find()
         .then((products)=>{
             //Filter out the information that you want from the array of documents that was returned into
             //a new array
@@ -67,6 +68,37 @@ router.post("/dashboard", isAuthenticated, (req,res) => {
     
         })
         .catch(err=>console.log(`Error happened when pulling from the database :${err}`));
+    }
+    else {
+        productsModel.find({category: req.body.category})
+        .then((products)=>{
+            //Filter out the information that you want from the array of documents that was returned into
+            //a new array
+            //Array 300 documents meaning that the array has 300 elements 
+            const filteredItems =  products.map(item=>{
+                    return {
+                        id: item._id,
+                        name: item.name,
+                        description:item.description,
+                        category : item.category,
+                        price : item.price,
+                        bestseller: item.bestseller,
+                        link: item.link,
+                        thumb: item.thumb,
+                        quantity: item.quantity
+                    }
+            });
+    
+            res.render("clerkDashboard",{
+               data : filteredItems
+            });
+    
+        })
+        .catch(err=>console.log(`Error happened when pulling from the database :${err}`));
+    }
+
+
+    
 });
 
 module.exports = router;
