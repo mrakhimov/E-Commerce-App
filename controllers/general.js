d95e670a58bafd7a5524371c2708dd1970051beb
mrakhimov <mokhinur.rakhimov@gmail.com>
2020-04-16 17:55:51 -0400

    Category implemented but has a bug

--- controllers/general.js
+++ controllers/general.js
@@ -40,4 +40,33 @@ router.get("/",(req,res)=>{
 // Handle dashboard only if authenticated
 router.get("/dashboard",isAuthenticated,dashBoardLoader);
 
+// Filter results by category
+router.post("/dashboard", isAuthenticated, (req,res) => {
+    productsModel.find({category: req.body.category})
+        .then((products)=>{
+            //Filter out the information that you want from the array of documents that was returned into
+            //a new array
+            //Array 300 documents meaning that the array has 300 elements 
+            const filteredItems =  products.map(item=>{
+                    return {
+                        id: item._id,
+                        name: item.name,
+                        description:item.description,
+                        category : item.category,
+                        price : item.price,
+                        bestseller: item.bestseller,
+                        link: item.link,
+                        thumb: item.thumb,
+                        quantity: item.quantity
+                    }
+            });
+    
+            res.render("clerkDashboard",{
+               data : filteredItems
+            });
+    
+        })
+        .catch(err=>console.log(`Error happened when pulling from the database :${err}`));
+});
+
 module.exports = router;
